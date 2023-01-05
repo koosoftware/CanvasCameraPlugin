814e1115c03a39e4e76cea3cd28ee8893f07128f
koo <koomingchin@gmail.com>
2023-01-06 01:01:12 +0800

    Fix getDeviceList callback

--- www/CanvasCamera.d.ts
+++ www/CanvasCamera.d.ts
@@ -109,7 +109,7 @@ export interface CanvasCamera {
     initialize(fcanvas: HTMLCanvasElement | CanvasCameraCanvasElements, tcanvas?: HTMLCanvasElement): CanvasCamera;
     start(userOptions: CanvasCameraUserOptions, onError?: CanvasCameraPluginResultCallbackFunction, onSuccess?: CanvasCameraPluginResultCallbackFunction): CanvasCamera;
     stop(onError?: CanvasCameraPluginResultCallbackFunction, onSuccess?: CanvasCameraPluginResultCallbackFunction): CanvasCamera;
-    getDeviceList(): CanvasCamera[];
+    getDeviceList(onError?: CanvasCameraPluginResultCallbackFunction, onSuccess?: CanvasCameraPluginResultCallbackFunction): CanvasCamera;
     flashMode(flashMode: boolean, onError?: CanvasCameraPluginResultCallbackFunction, onSuccess?: CanvasCameraPluginResultCallbackFunction): CanvasCamera;
     cameraPosition(cameraFacing: CanvasCameraCameraFacing, onError?: CanvasCameraPluginResultCallbackFunction, onSuccess?: CanvasCameraPluginResultCallbackFunction): CanvasCamera;
     capture(data: CanvasCameraData): CanvasCamera;
